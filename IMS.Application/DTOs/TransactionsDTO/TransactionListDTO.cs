f72253983e73ca669c25172f5356c0e59b0c5e97
Adel Muhammad <141270987+AdelMohamed23@users.noreply.github.com>
2025-02-28 16:59:53 +0200

    Transaction History is maintained for audit purposes, showing the date, quantity, and type of transaction (sale or purchase).

---
+++ IMS.Application/DTOs/TransactionsDTO/TransactionListDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace IMS.Application.DTOs.TransactionsDTO
+{
+    public class TransactionListDTO : TransactiontDTO
+    {
+    }
+}
---
+++ IMS.Application/Mapping/TransactionsMapping/QueryMapping/GetTransactionByIdQueryMapping.cs
@@ -0,0 +1,14 @@
+﻿using IMS.Application.DTOs.TransactionsDTO;
+using IMS.Domain.Entities;
+
+namespace IMS.Application.Mapping.TransactionsMapping
+{
+    public partial class TransactionsProfile
+    {
+        public void GetTransactionByIdQueryMapping()
+        {
+            CreateMap<Transaction, TransactiontDTO>();
+
+        }
+    }
+}
